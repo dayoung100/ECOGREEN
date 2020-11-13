f6ffec885ef93d0ff1dc3836d609e278416c0ee7
hyojin2 <34567074+hyojin2@users.noreply.github.com>
2020-11-13 21:29:11 +0900

    Update changeName.py

--- webcrawling/changeName.py
+++ webcrawling/changeName.py
@@ -6,7 +6,7 @@ file_names = os.listdir(file_path)
 i=1
 for name in file_names:
     src=os.path.join(file_path, name)
-    dst = 'wardrobe_' + str(i) + '.jpg' // rename file
+    dst = 'wardrobe' + str(i) + '.jpg' // rename file
     dst = os.path.join(file_path, dst)
     os. rename(src, dst)
     i+=1
