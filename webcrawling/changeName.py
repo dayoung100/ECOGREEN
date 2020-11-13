fddbc960d8272cb4d01e0e0acd26e7cb3a322b2d
hyojin2 <34567074+hyojin2@users.noreply.github.com>
2020-11-13 21:02:08 +0900

    Create changeName.py
    Change file name

---
+++ webcrawling/changeName.py
@@ -0,0 +1,12 @@
+import os
+
+file_path='C:/Users/user/Desktop/crawling/크롤링사진'
+file_names = os.listdir(file_path)
+
+i=1
+for name in file_names:
+    src=os.path.join(file_path, name)
+    dst = 'wardrobe_' + str(i) + '.jpg' // change file name
+    dst = os.path.join(file_path, dst)
+    os. rename(src, dst)
+    i+=1
